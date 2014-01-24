8372bd7660cea9715d8c2daf0796b2302647962b
Rory Crispin <rcqikmail@gmail.com>
2014-01-24 18:30:19 +0000

    Update BlueComms.java

--- src/com/rozzles/torocam/BlueComms.java
+++ src/com/rozzles/torocam/BlueComms.java
@@ -1,5 +1,5 @@
 /*
- android gr* Rory Crispin --rorycrispin.co.uk -- rozzles.com
+ * Rory Crispin --rorycrispin.co.uk -- rozzles.com
  * 
  * Distributed under the Creative Commons 
  * Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
